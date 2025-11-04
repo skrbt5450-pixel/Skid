27b00bd8904c2e4c77b437c8a56e2211a272a6a5
skrbt5450-pixel <skrbt5450@gmail.com>
2025-11-04 22:50:04 +0300

    Update Tiktok.py

--- Tiktok.py
+++ Tiktok.py
@@ -1,1 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
 
+def get_country(username):
+    url = f"https://www.tiktok.com/@{username}"
+    response = requests.get(url)  soup = BeautifulSoup(response.text, 'html.parser')   bio = soup.find('div', {'class': 'tiktok-y0c9h8-ProfileHeader-module_bioContainer___15XVt'}).text
+    country = None
+    if"Country" in bio:
+        country = bio.split("Country:")[1].split(" ")[0]    return country
